7567060b3bacdb023ee9e3161b1d9b2f3539cd67
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2022-07-22 09:35:30 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import '../styles/global.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { useEffect } from 'react'
 import { config } from '@fortawesome/fontawesome-svg-core'
-import { init } from '@socialgouv/matomo-next'
 config.autoAddCss = false;
 
-const MATOMO_URL = '//80.209.230.81/'
-const MATOMO_SITE_ID = '3'
-
 export default function App({ Component, pageProps }) {
-    useEffect(() => {
-        init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID })
-    }, [])
     
     return <Component {...pageProps} />
 }
