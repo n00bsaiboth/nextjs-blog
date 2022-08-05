a1db48204e6653b21d13286bcb1b34cc78d61543
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2022-08-05 10:12:57 +0300

    Added matomo to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
+import React, { useEffect } from "react";
 import '../styles/global.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 config.autoAddCss = false;
 
+import { init } from "@socialgouv/matomo-next";
+
+const MATOMO_URL = '//matomo.testi-pannu.net';
+const MATOMO_SITE_ID = 7;
+
 export default function App({ Component, pageProps }) {
+    useEffect(() => { 
+        init({url: MATOMO_URL, siteId: MATOMO_SITE_ID});
+    }, []);
     return <Component {...pageProps} />
 }
