711ab837c2ff14dfd821b19cc1b3746a428a58f6
Jussi Jokinen <jussi.k.jokinen@gmail.com>
2024-04-09 13:25:37 +0300

    NB-1: Added sample workflows and pipelines and edited the content

--- pages/index.js
+++ pages/index.js
@@ -31,29 +31,26 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
           <p className={utilStyles.intro}>
             <FontAwesomeIcon icon={faQuoteLeft}></FontAwesomeIcon>
-            {' '} Freelancer | Script kiddie | Raspberry Pi | IoT | Web developer | Linux and information security enthusiast.  {' '} 
+            {' '} Sunday coder | Freelancer | Script kiddie | Raspberry Pi | IoT | Web developer | Linux and information security enthusiast. {' '} 
             <FontAwesomeIcon icon={faQuoteRight}></FontAwesomeIcon> 
           </p> 
       </section>
 
-      <section>
+      <section id="intro">
         <p>My interests range from Open Source to Information Security. I am also interested in Cryptocurrencies, and zeitgeist movement. </p>
-
-        <p>I'm currently working at Citrus Solutions, as a Drupalist. You can consider me as a full-stack developer, but usually I work on the backend, because I don't want to be the center of attention (that was actually joke, because here in Finland we have this thing called, "puujalkavitsit"). </p>
-        
-        <p>Every now and then I'm a light entrepreneuer, mostly helping my neighbors around or then again, because of my background, also do some LAMP-projects, mostly wordpress.</p>
-
+        <p>You can consider me as a full-stack developer, but usually I work on the backend, because backend feels more like home to me. </p>
+        <p>Every now and then I'm a light entrepreneuer, mostly helping my neighbors around or then again, because of my background, also do some LAMP-projects, mostly Drupal and Wordpress.</p>
         <p>I'm also a member of the Pirate Party, COSS and EFFI.</p>
-        <p>Notice: That now that I work full time, I don't have the time to work on this blog. </p>
+        <p>Current status: Looking for a new job.</p>
       </section>
 
-      <section>
-            <p><a href="mailto: jussi.k.jokinen [at] gmail.com"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>{' '}jussi.k.jokinen [at] gmail.com</a></p>
-            <p><a href="tel:+358 41 701 6628"><FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>{' '}  +358 41 701 6628</a></p>
-            <p><a href="https://www.linkedin.com/in/jussi-k-jokinen/" target="_blank"><FontAwesomeIcon icon={faLink}></FontAwesomeIcon>{' '}linkedin.com/jussi-k-jokinen</a></p>
+      <section id="contact">
+        <p><a href="mailto: jussi.k.jokinen [at] gmail.com" title="jussi.k.jokinen [at] gmail.com" rel="noreferrer noopener"><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>{' '}jussi.k.jokinen [at] gmail.com</a></p>
+        <p><a href="tel:+358 41 701 6628" title="+358 41 701 6628" rel="noreferrer noopener"><FontAwesomeIcon icon={faPhone}></FontAwesomeIcon>{' '}  +358 41 701 6628</a></p>
+        <p><a href="https://www.linkedin.com/in/jussi-k-jokinen/" target="_blank" title="Jussi Jokinen [at] linkedin" rel="noreferrer noopener"><FontAwesomeIcon icon={faLink}></FontAwesomeIcon>{' '}Jussi Jokinen [at] linkedin</a></p>
       </section>
       
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <section id="blog" className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -70,10 +67,8 @@ export default function Home({ allPostsData }) {
         </ul>
       </section>
 
-      <section className={utilStyles.footer}>
-        <p>
-          This blog is powered by: <a href="https://nextjs.org/learn" target="_blank">Next.js</a>.
-        </p>
+      <section id="footer" className={utilStyles.footer}>
+        <p>This blog is powered by: <a href="https://nextjs.org/learn" target="_blank" title="Next.js" rel="noreferrer noopener">Next.js</a>.</p>
       </section>
     </Layout>
   )
