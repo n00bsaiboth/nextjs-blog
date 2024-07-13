19a8fee9a57bbdf6bd86ff9d4e8bec4e1394a126
Jussi Jokinen <jussi.jokinen@citrus.fi>
2024-07-13 07:28:50 +0300

    develop: setting up matomo

--- pages/index.js
+++ pages/index.js
@@ -27,6 +27,21 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
+        <!-- Matomo -->
+        <script>
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//matomo.openinnovations.io/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+      <!-- End Matomo Code -->
       </Head>
       <section className={utilStyles.headingMd}>
           <p className={utilStyles.intro}>
