c296f1bd93dae347fc66fd0b6fe16816f8dc954f
Jussi Jokinen <jussi.jokinen@citrus.fi>
2024-07-13 07:39:42 +0300

    develop: setting up matomo, third time is the charm

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ config.autoAddCss = false;
 import { init } from "@socialgouv/matomo-next";
 import { push } from "@socialgouv/matomo-next";
 
-const MATOMO_URL = '//matomo.testi-pannu.net';
+const MATOMO_URL = '//matomo.openinnovations.io';
 const MATOMO_SITE_ID = 3;
 
 export default function App({ Component, pageProps }) {
--- pages/index.js
+++ pages/index.js
@@ -27,19 +27,6 @@ export default function Home({ allPostsData }) {
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
-        <script>
-          var _paq = window._paq = window._paq || [];
-          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-          _paq.push(['trackPageView']);
-          _paq.push(['enableLinkTracking']);
-          (function() {
-            var u="//matomo.openinnovations.io/";
-            _paq.push(['setTrackerUrl', u+'matomo.php']);
-            _paq.push(['setSiteId', '3']);
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-          })();
-        </script>
       </Head>
       <section className={utilStyles.headingMd}>
           <p className={utilStyles.intro}>
